9d3f4f0143db5f291e536fb671c05ccd6364bef4
TuyenNguyenDinh <tuyennguyendinh1608@gmail.com>
2020-12-30 08:07:05 +0700

    add list category, add repository and update layout

--- app/src/main/java/com/example/doanandroid02/ui/category/CategoryFragment.java
+++ app/src/main/java/com/example/doanandroid02/ui/category/CategoryFragment.java
@@ -1,32 +1,35 @@
-package com.example.doanandroid02.ui.home;
+package com.example.doanandroid02.ui.category;
 
+import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.AdapterView;
+import android.widget.ListView;
 
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
-import androidx.recyclerview.widget.LinearLayoutManager;
-import androidx.recyclerview.widget.RecyclerView;
+
 
 import com.example.doanandroid02.R;
 import com.example.doanandroid02.activity.MainContract;
 import com.example.doanandroid02.activity.MainPresenter;
-import com.example.doanandroid02.adapter.NewProductAdapter;
+import com.example.doanandroid02.adapter.CategoryAdapter;
 import com.example.myapplicationbottmi.models.Category;
 import com.example.myapplicationbottmi.models.Product;
 
 import java.util.List;
 
-public class HomeFragment extends Fragment implements MainContract.View {
+public class CategoryFragment extends Fragment implements MainContract.View{
 
-    RecyclerView recyclerView;
-    NewProductAdapter newProductAdapter;
     MainContract.Presenter mPresenter;
+    ListView listView;
+    CategoryAdapter categoryAdapter;
+
 
     @Override
     public void onCreate(@Nullable Bundle savedInstanceState) {
@@ -34,19 +37,20 @@ public class HomeFragment extends Fragment implements MainContract.View {
         setHasOptionsMenu(true);
     }
 
-
-
     @Override
     public void onCreateOptionsMenu(@NonNull Menu menu, @NonNull MenuInflater inflater) {
         super.onCreateOptionsMenu(menu, inflater);
-        getActivity().getMenuInflater().inflate(R.menu.menu_main, menu);
+        getActivity().getMenuInflater().inflate(R.menu.menu, menu);
     }
 
-    public View onCreateView(@NonNull LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
-        View view = inflater.inflate(R.layout.fragment_home, container, false);
-        recyclerView = view.findViewById(R.id.recyclerviewHome);
+    @Nullable
+    @Override
+    public View onCreateView(@NonNull LayoutInflater inflater, @Nullable ViewGroup container, @Nullable Bundle savedInstanceState) {
+        View view = inflater.inflate(R.layout.fragment_category,container,false);
+        listView = view.findViewById(R.id.listview);
+
         mPresenter = new MainPresenter(this);
-        mPresenter.loadProducts();
+        mPresenter.loadCategories();
         return view;
     }
 
@@ -62,15 +66,14 @@ public class HomeFragment extends Fragment implements MainContract.View {
 
     @Override
     public void updateListProduct(List<Product> products) {
-        newProductAdapter = new NewProductAdapter(products, getActivity());
-        recyclerView.setHasFixedSize(true);
-        recyclerView.setLayoutManager(new LinearLayoutManager(getActivity(), LinearLayoutManager.HORIZONTAL,false));
-        recyclerView.setAdapter(newProductAdapter);
+
     }
 
     @Override
     public void updateListCategories(List<Category> categories) {
-
+        categoryAdapter = new CategoryAdapter(categories, getActivity());
+        listView.setAdapter(categoryAdapter);
     }
+
 
 }
